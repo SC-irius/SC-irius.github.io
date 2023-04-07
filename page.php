0a8ba707ba43c86d778249522e4777179318cf90
SC-irius X <128828827+SC-irius@users.noreply.github.com>
2023-04-07 14:15:17 +0200

    Update page.php

--- page.php
+++ page.php
@@ -1,6 +1,5 @@
 <?php
-  // On récupère les variables depuis le formulaire HTML
-  $rue = $_GET["rue"];
+  // On récupère les variables depuis le formulaire HTML 
   $rue = $_GET["rue"];
   $prix = $_GET["prix"];
   $expected_income = $_GET["expected_income"];
@@ -19,20 +18,23 @@
   $q_eau_ville = $_GET["q_eau_ville"];
   $nb_immigrations = $_GET["nb_immigrations"];
   $t_vacances_locatives = $_GET["t_vacances_locatives"];
-?>
-<html>
-<head>
-<title>Résultat du script Python</title>
-</head>
-<body>
-<!-- CORPS -->
-<div id="resultat">
-<p>Voici le résultat du script python :</p>
-<?php
-  // On exécute le script python en lui passant les variables comme arguments
-  // et on affiche directement sa sortie
-  passthru("python realt_metrics_usa.py $rue $prix $expected_income $surface $zone_prix_m2 $age $presence_ecoles $presence_transports $presence_commerces $zone_inondable $s8 $s42 $type_bien $etat $tendance_marche_immobilier $q_eau_ville $nb_immigrations $t_vacances_locatives ");
-?>
-</div>
-</body>
+?> 
+<html> 
+<head> 
+<title>Résultat du script Python</title> 
+</head> 
+<body> 
+<!-- CORPS --> 
+  
+<div id=“resultat”> 
+<p>Voici le résultat du script python :</p> 
+<?php 
+  // On exécute le script python en lui passant les variables comme arguments 
+  // et on récupère sa sortie dans un tableau 
+  exec("python realt_metrics_usa.py $rue $prix $expected_income $surface $zone_prix_m2 $age $presence_ecoles $presence_transports $presence_commerces $zone_inondable $s8 $s42 $type_bien $etat $tendance_marche_immobilier $q_eau_ville $nb_immigrations $t_vacances_locatives ", $output); 
+  // On affiche le contenu du tableau 
+  foreach ($output as $line) { echo $line . “<br>”; } 
+?> 
+</div> 
+</body> 
 </html>
